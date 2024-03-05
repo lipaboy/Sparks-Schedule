74da9eb8cc0c5445ab7ffcdce33d90b5eedd87df
G01111100 <145698991+G01111100@users.noreply.github.com>
2024-03-05 15:28:07 +0700

    Реализован вывод таблиц в excel-файл

--- excel/mainExcel.py
+++ excel/mainExcel.py
@@ -1,78 +1,81 @@
 import openpyxl
 from SparksScheduleSearch import SparksScheduleSearch
 
-def read_table_xlsx(filename):
-    print("\n\tread_table_xlsx")
-    wb = openpyxl.load_workbook(filename = filename)
-    sheet = wb['Лист1']
+def formatting_cell(sheet, row, column, value, fontSize, fontName, fontBold, fontItalic, alignmentHorizontal, alignmentVertical):
+    sheet.cell(row=row, column=column).value = value
+    sheet.cell(row=row, column=column).font = openpyxl.styles.Font(size=fontSize, name=fontName, bold=fontBold, italic=fontItalic)
+    sheet.cell(row=row, column=column).alignment = openpyxl.styles.Alignment(horizontal=alignmentHorizontal, vertical=alignmentVertical)
 
-    # strCell = sheet.cell(row=1, column=1).value #Begin at 1, not at 0
-    # print(strCell)
-    # print(sheet.max_column)
-    # print(sheet.max_row)
-    # # vals = [v[0].value for v in sheet['A1:C26']]
-    # for numRow in vals:
-    #     print(numRow)
-    # print(sheet['C21'].value)
-    for numRow in range(1, sheet.max_row + 1):
-        bufferRow = list(range(sheet.max_column))
-        for numColumn in range(1, sheet.max_column + 1):
-            bufferRow[numColumn-1] = sheet.cell(row=numRow, column=numColumn).value
-        print(bufferRow)
 
-def write_table_xlsx(filename, newFilename, sheetName):
-    print("\n\twrite_table_xlsx")
-    wb = openpyxl.load_workbook(filename=filename)
-    sheet = wb['Лист1']
-    newWb = openpyxl.Workbook()
-    newSheet = newWb.worksheets[0]
-    newSheet.title = sheetName
-    # del newWb["Sheet"]
-    # newSheet = newWb.create_sheet("Test")
-    # print(newWb.sheetnames)
-    numRow = 1
-    for numColumn in range(1, sheet.max_column + 1):
-        if numColumn == 1:
-            newSheet.column_dimensions[openpyxl.utils.get_column_letter(numColumn)].width = 10
-        elif numColumn == 2:
-            newSheet.column_dimensions[openpyxl.utils.get_column_letter(numColumn)].width = 7
-        else:
-            newSheet.column_dimensions[openpyxl.utils.get_column_letter(numColumn)].width = 20
-
-        newSheet.cell(row=numRow, column=numColumn).font = openpyxl.styles.Font(bold=True)
-        newSheet.cell(row=numRow, column=numColumn).alignment = openpyxl.styles.Alignment(horizontal='center')
-        newSheet.cell(row=numRow, column=numColumn).value = sheet.cell(row=numRow, column=numColumn).value
-    for numRow in range(2, sheet.max_row + 1):
-        for numColumn in range(1, sheet.max_column + 1):
-            newSheet.cell(row=numRow, column=numColumn).font = openpyxl.styles.Font(italic=True)
-            if numColumn == 1:
-                bufferAlignment = openpyxl.styles.Alignment(horizontal='right')
-            elif numColumn == 2:
-                bufferAlignment = openpyxl.styles.Alignment(horizontal='center')
-            else:
-                bufferAlignment = openpyxl.styles.Alignment(horizontal='left')
-            newSheet.cell(row=numRow, column=numColumn).alignment = bufferAlignment
-            newSheet.cell(row=numRow, column=numColumn).value = sheet.cell(row=numRow, column=numColumn).value
-
-    newWb.save(newFilename)
-    print("New file save!")
-
-# read_table_xlsx("testTable.xlsx")
-# write_table_xlsx("testTable.xlsx", "newTestTable.xlsx", "Test")
-
+filename = "Timetable.xlsx"
+wb = openpyxl.Workbook()
+sheet = wb.worksheets[0]
+sheet.title = "sheetName"
+#Todo Высчитывание числа и месяца
+#--Init
 sparks = SparksScheduleSearch()
-data = sparks.findFirstGhost()
-for i in range(len(data)):
-    print(i)
-    for k in data[i].items():
-        print(k[0].Name, k[1])
-    print()
+timeTable = sparks.findFirstGhost()
+mediumBorder = openpyxl.styles.Side(border_style="medium", color="000000")
+thickBorder = openpyxl.styles.Side(border_style="thick", color="000000")
+week = {1: 'ПН',
+        2: 'ВТ',
+        3: "СР",
+        4: "ЧТ",
+        5: "ПТ",
+        6: "СБ",
+        7: "ВС"}
+staff = dict()
+i = 0
+for s in timeTable[0].items():
+    staff[s[0].Name] = i
+    i += 1
 
-# for i in data:  # i dict
-#     for k in i.items():
-#         print(k[0].Name, k[1])
-#         # print(k[0].Name)#EmployeeCard
-#         # print(k[1])#list[tuple[DayType, ShiftLength, PlaceToWork]]
-#     print()
-
-print("\n---END---")
+spaceBetweenTables = 1
+numberOfTablesInLine = 3
+tableWidth = 1 + len(week) + spaceBetweenTables  #+1 - begin from 1;+1 - space between tables
+tableHeight = 1 + len(staff) + spaceBetweenTables
+#--
+#--Fill table all timeTables
+for numOfTable in range(len(timeTable)):
+    startingPointColumn = 1 + ((numOfTable % numberOfTablesInLine) * tableWidth)
+    startingPointRow = 1 + ((numOfTable // numberOfTablesInLine) * tableHeight)
+    sheet.column_dimensions[openpyxl.utils.get_column_letter(startingPointColumn)].width = 10
+    formatting_cell(sheet, startingPointRow, startingPointColumn, f"№ {numOfTable + 1}", 14, "Times New Roman", False, False, "center", "center")
+    #--Formatting and x-filling the timetable
+    for row in range(startingPointRow+1, len(staff)+startingPointRow+1):
+        for column in range(startingPointColumn+1, len(week)+startingPointColumn+1):
+            sheet.cell(row=row, column=column).border = openpyxl.styles.Border(right=mediumBorder,bottom=mediumBorder)
+            formatting_cell(sheet, row, column, '✕', 14, "Times New Roman", False, False, "center", "center")
+    #--
+    #--Creating of header of timetable
+    for day in range(1, len(week)+1):
+        sheet.cell(row=startingPointRow, column=startingPointColumn+day).border = openpyxl.styles.Border(left=thickBorder, right=thickBorder, top=thickBorder, bottom=thickBorder)
+        formatting_cell(sheet, startingPointRow, startingPointColumn+day, week[day], 14, "Times New Roman", True, False, "center", "center")
+    staffNames = list(staff.keys())
+    for memberOfStaff in range(len(staff)):
+        sheet.cell(row=startingPointRow + memberOfStaff + 1, column=startingPointColumn).border = openpyxl.styles.Border(left=thickBorder, right=thickBorder, top=thickBorder, bottom=thickBorder)
+        formatting_cell(sheet, startingPointRow + memberOfStaff + 1, startingPointColumn, staffNames[memberOfStaff], 14, "Times New Roman", True, True, "right", "center")
+    #--
+    #--Fill data to the timetable
+    typeOfShift = '✕'#'✖'
+    # print(numOfTable+1)
+    for memberOfStaff in timeTable[numOfTable].items():  #dict[EmployeeCard, list[tuple[DayType, ShiftLength, PlaceToWork]]]
+        # print(memberOfStaff[0].Name, memberOfStaff[1])
+        for i in memberOfStaff[1]: #list[tuple[DayType, ShiftLength, PlaceToWork]]
+            if i[2] == 'Hall':
+                sheet.cell(row=startingPointRow + staff[memberOfStaff[0].Name] + 1, column=startingPointColumn + i[0]).fill = openpyxl.styles.PatternFill(fill_type='solid', fgColor='F9E79F')
+                if i[1] == 1:
+                    typeOfShift = 'С'
+                else:
+                    typeOfShift = 'С2'
+            else:
+                sheet.cell(row=startingPointRow + staff[memberOfStaff[0].Name] + 1, column=startingPointColumn + i[0]).fill = openpyxl.styles.PatternFill(fill_type='solid', fgColor='ABEBC6')
+                typeOfShift = 'T'
+            formatting_cell(sheet, startingPointRow + staff[memberOfStaff[0].Name] + 1, startingPointColumn + i[0], typeOfShift, 14, "Times New Roman", False, False, "center", "center")
+    #--
+    # print('\t----------')
+#--
+#--Close and save excel's file
+wb.save(filename)
+print("\n\t\tNew file save!")
+#--
