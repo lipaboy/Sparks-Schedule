789e5019c4d26542151de717d4e3fd07423f5c6e
Artem <lipatkin96@gmail.com>
2024-04-19 16:51:55 +0700

    Парочка изменений

--- search/draft.py
+++ search/draft.py
@@ -1,7 +1,23 @@
-import itertools
-alphabet = "РЕКА"
-# ar = itertools.product(alphabet, repeat=4) #Размещение с повторением
-ar = itertools.permutations(alphabet, 4)
-l = sorted(list(ar))
-for i in range(len(l)):
-    print(i + 1, l[i])
+"""x = 208
+y = 192
+z = x & y
+
+print(bin(x))
+print(bin(y))
+print(bin(z))
+print(z)
+
+
+t = 255
+print(bin(t))
+t = t << 1
+print(bin(t))"""
+
+c=85
+g=89
+m=240
+z = c & m
+n = g & m
+print(z,n)
+
+
